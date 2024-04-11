e868f4faa1afb22855f4c2fb1e5f6925e3ae3156
Jacques Mayol <jacques@ocean.sea>
2024-04-11 12:35:51 +0200

    Use pr_info

--- module.c
+++ module.c
@@ -1,20 +1,26 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/module.h>
+
+#include <linux/module.h> /* Needed by all modules */
+#include <linux/printk.h> /* Needed for pr_info() */
 
 MODULE_DESCRIPTION("My kernel module");
-MODULE_AUTHOR("Me");
+MODULE_AUTHOR("Jacques");
 MODULE_LICENSE("GPL");
 
 static int init_module(void)
 {
-    printk(KERN_INFO "Hi");
+    // printk(KERN_INFO "Hi");
+    pr_info("Hello\n");
+
+    /* A non 0 return means init_module failed; module can't be loaded. */
     return 0;
 }
 
 static void cleanup_module(void)
 {
-    printk(KERN_INFO "Bye");
+    // printk(KERN_INFO "Bye");
+    pr_info("Goodbye\n");
 }
 
 module_init(init_module);
